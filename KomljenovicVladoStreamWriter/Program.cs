522d092df1ef46341bc9f7a516d5f9675abab712
VladoKomljenovic <44942108+VladoKomljenovic@users.noreply.github.com>
2019-01-16 17:41:18 +0100

    Add files via upload

---
+++ KomljenovicVladoStreamWriter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CitanjeTeksta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader
+                (
+@"C:\Users\Učenik\Documents");
+            // Čitamo datoteku liniju po liniju
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            // Zatvaramo datoteku
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
